7ae9b302238a9215118db63ed399d599ef81d734
Toilal <toilal.dev@gmail.com>
2014-02-15 11:02:30 +0100

    Update babelfish version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='babelfish',
-    version='0.5.1',
+    version='0.5.1.memory0',
     license='BSD',
     description='A module to work with countries and languages',
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
