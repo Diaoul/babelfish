ce9377c54259a2b8a9d070903cbffac6f6332e92
Nicolas Wack <wackou@gmail.com>
2014-06-22 12:18:53 +0200

    Added python 3.4 to the list of pypi trove classifiers

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(name='babelfish',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Multimedia :: Video',
         'Topic :: Software Development :: Libraries :: Python Modules'],
     test_suite='babelfish.tests.suite')
