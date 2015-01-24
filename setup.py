3a177fb4097b971ce32522fdfc96785be24761dd
Toilal <toilal.dev@gmail.com>
2015-01-24 09:31:11 +0100

    Switched to version 0.5.5-dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='babelfish',
-    version='0.5.4',
+    version='0.5.5-dev',
     license='BSD',
     description='A module to work with countries and languages',
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
